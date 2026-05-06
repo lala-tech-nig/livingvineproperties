4e30860b9ca0248da851c43cae9779904de27224
LALA TECH <olaniyanshamsudeenyinka001@gmail.com>
2026-05-06 12:51:29 +0100

    new update to the backend

--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
